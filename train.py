50e769d75dfeadcb783a1638cf7ea952b0898e90
Pierre-Hadrien Levieil <pierre-hadrien.levieil@epfl.ch>
2023-03-01 11:42:10 +0100

    works, no self attention

--- train.py
+++ train.py
@@ -1,22 +1,26 @@
-import torch  # we use PyTorch: https://pytorch.org
+import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-torch.manual_seed(1337)
-
-batch_size = 4  # how many independent sequences will we process in parallel?
+# hyperparameters
+batch_size = 32  # how many independent sequences will we process in parallel?
 block_size = 8  # what is the maximum context length for predictions?
-max_iters = 3000
+max_iters = 1000
 eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
 
-# read it in to inspect it
+# ------------
+
+torch.manual_seed(1337)
+
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
+# here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 # create a mapping from characters to integers
@@ -25,14 +29,14 @@ itos = {i: ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]  # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
 
-# let's now encode the entire text dataset and store it into a torch.Tensor
+# Train and test splits
 data = torch.tensor(encode(text), dtype=torch.long)
-# Let's now split up the data into train and validation sets
 n = int(0.9 * len(data))  # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
 
+# data loading
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
@@ -41,6 +45,7 @@ def get_batch(split):
     y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
+
 
 @torch.no_grad()
 def estimate_loss():
@@ -57,6 +62,7 @@ def estimate_loss():
     return out
 
 
+# super simple bigram model
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
@@ -67,13 +73,14 @@ class BigramLanguageModel(nn.Module):
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
+
         B, T = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx)  # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
-        x = tok_emb + pos_emb  # (B,T,C)
-        logits = self.lm_head(x)  # (B,T,vocab_size)
+        #pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T,C)
+        #x = tok_emb + pos_emb  # (B,T,C)
+        logits = self.lm_head(tok_emb)  # (B,T,vocab_size)
 
         if targets is None:
             loss = None
@@ -107,12 +114,12 @@ m = model.to(device)
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-for steps in range(max_iters):
+for iter in range(max_iters):
 
     # every once in a while evaluate the loss on train and val sets
-    if steps % eval_interval == 0:
+    if iter % eval_interval == 0:
         losses = estimate_loss()
-        print(f"step {steps}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     # sample a batch of data
     xb, yb = get_batch('train')
@@ -123,7 +130,6 @@ for steps in range(max_iters):
     loss.backward()
     optimizer.step()
 
-print(loss.item())
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
